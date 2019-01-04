1c6a0e8a785383bbb10cc1898108d37d5051a6a7
CPPtrueCoder <45642168+CPPtrueCoder@users.noreply.github.com>
2019-01-05 02:05:35 +0300

    Update  Functions.cpp to Transformations.cpp
    Created Input_vector() , Input_angle (); 
    In Input_angle () : 
    Check for types  : user can't input symbols ; 
    In Velocity_to_bound :
    created the constant for size of vectors and arrays 
    The bound vector return from function

---
+++ Transformations.cpp
@@ -0,0 +1,59 @@
+
+#include "Header.hpp"
+
+double Input_angle()
+{   std::cout<<"Get the Attack angle and Slip angle ";
+    double angle ;
+    using std::cin;
+    using std::cout;
+    
+    while (true){
+        if (cin>>angle){
+            return angle ;
+        }
+        else{
+            cout<< " Bad value , input no symbols"<<std::endl;
+            cin.clear();
+            cin.get();
+            continue;
+        }
+    }
+    return angle ;
+}
+vector Input_vector()
+{   auto size  =3;
+    std::cout<<" Get the coordinates of vector: ";
+    vector your_vector(size);
+    for (int line = 0 ; line<size;++line)
+    {
+        std::cin>>your_vector[line];
+    }
+    return your_vector;
+}
+vector  Velocity_to_bound (const vector &velo_vec ,double Attack_angle , double Slip_angle )
+{   const int size = 3;
+    vector bound_vec(size);
+    double trans_matr[size][size]={0};
+    trans_matr[0][0]=cos(Attack_angle)*cos(Slip_angle);
+    trans_matr[0][1]=sin(Attack_angle);
+    trans_matr[0][2]=-cos(Attack_angle)*sin(Slip_angle);
+    trans_matr[1][0]=-sin(Attack_angle)*cos(Slip_angle);
+    trans_matr[1][1]=cos(Attack_angle);
+    trans_matr[1][2]=sin(Attack_angle)*sin(Slip_angle);
+    trans_matr[2][0]=sin(Slip_angle);
+    trans_matr[2][1]=0.0;
+    trans_matr[2][2]=cos(Slip_angle);
+    for (int line=0;line<size;++line){
+        for (int col=0;col<size;++col){
+            
+            bound_vec[line]+=trans_matr[line][col]*velo_vec[col];
+        }
+    }
+    return bound_vec;
+}
+
+void Show_vec (const vector &vector){
+    for (auto coordinate : vector)
+        std::cout<<coordinate<<std::endl;
+}
+
