36724824bdeef6e5e7737c3ade81dc17d7580cf4
CPPtrueCoder <45642168+CPPtrueCoder@users.noreply.github.com>
2019-01-05 01:50:22 +0300

    Update main.cpp
    Used functional programming . 
    Cut function main from 23 strings to 5

--- main.cpp
+++ main.cpp
@@ -1,27 +1,8 @@
-
-
-#include <iostream>
-#include "Coordinate.hpp"
+#include "Header.hpp"
 int main() {
-  using std::cout;
-  using std::cin;
-  int size = 3;
-  Coordinates velo_vec(size);
-  for (int i = 0; i < size; ++i) {
-    cout << " Take the coordinate:";
-    cin >> velo_vec[i];
-  }
-  Coordinates bound(size);
-  cout << " Get the the slip angle:"
-       << " ";
-  double Slip_angle;
-  cin >> Slip_angle;
-  cout << "Get the angle of attack:"
-       << " ";
-  double Attack_angle;
-  cin >> Attack_angle;
-  Velocity_to_bound(velo_vec, bound, Slip_angle, Attack_angle);
-  cout << " The velocity vector in bound system coordinate : ";
-  Show_vec(bound);
-  return 0;
+    double Attack_angle=Input_angle();
+    double Slip_angle=Input_angle();
+    vector velo_vec = Input_vector();
+    vector bound_vec =Velocity_to_bound(velo_vec, Attack_angle, Slip_angle);
+    Show_vec(bound_vec);
 }
