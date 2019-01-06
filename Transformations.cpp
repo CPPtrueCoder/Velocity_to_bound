3da177f8351b19c2aebdc7517b025c18c6c171d2
CPPtrueCoder <45642168+CPPtrueCoder@users.noreply.github.com>
2019-01-06 18:33:57 +0300

    Update Transformations.cpp
    Added : 
    Function for Input Matrix for transformation .

--- Transformations.cpp
+++ Transformations.cpp
@@ -1,9 +1,10 @@
 
-#include "Header.hpp"
+
+#include "Transformation.hpp"
 
 double Input_angle()
 {   std::cout<<"Get the Attack angle and Slip angle ";
-    double angle ;
+    double angle=0.0 ;
     using std::cin;
     using std::cout;
     
@@ -30,19 +31,10 @@ vector Input_vector()
     }
     return your_vector;
 }
-vector  Velocity_to_bound (const vector &velo_vec ,double Attack_angle , double Slip_angle )
+vector  Velocity_to_bound (const vector &velo_vec,double trans_matr[3][3] ,double Attack_angle , double Slip_angle )
 {   const int size = 3;
     vector bound_vec(size);
-    double trans_matr[size][size]={0};
-    trans_matr[0][0]=cos(Attack_angle)*cos(Slip_angle);
-    trans_matr[0][1]=sin(Attack_angle);
-    trans_matr[0][2]=-cos(Attack_angle)*sin(Slip_angle);
-    trans_matr[1][0]=-sin(Attack_angle)*cos(Slip_angle);
-    trans_matr[1][1]=cos(Attack_angle);
-    trans_matr[1][2]=sin(Attack_angle)*sin(Slip_angle);
-    trans_matr[2][0]=sin(Slip_angle);
-    trans_matr[2][1]=0.0;
-    trans_matr[2][2]=cos(Slip_angle);
+
     for (int line=0;line<size;++line){
         for (int col=0;col<size;++col){
             
@@ -56,4 +48,15 @@ void Show_vec (const vector &vector){
     for (auto coordinate : vector)
         std::cout<<coordinate<<std::endl;
 }
-
+void Input_matrix (double trans_matr[SIZE][SIZE],double Attack_angle,double Slip_angle)
+{
+    trans_matr[0][0]=cos(Attack_angle)*cos(Slip_angle);
+    trans_matr[0][1]=sin(Attack_angle);
+    trans_matr[0][2]=-cos(Attack_angle)*sin(Slip_angle);
+    trans_matr[1][0]=-sin(Attack_angle)*cos(Slip_angle);
+    trans_matr[1][1]=cos(Attack_angle);
+    trans_matr[1][2]=sin(Attack_angle)*sin(Slip_angle);
+    trans_matr[2][0]=sin(Slip_angle);
+    trans_matr[2][1]=0.0;
+    trans_matr[2][2]=cos(Slip_angle);
+}
