951a6ecedbb8a6855963c229e20fe91baae01ab9
CPPtrueCoder <45642168+CPPtrueCoder@users.noreply.github.com>
2019-01-06 18:35:40 +0300

    Update main.cpp
    Added : 
    Transformation from degrees to radians  in Attack_angle , Slip_ angle .

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
-#include "Header.hpp"
+#include "Transformation.hpp"
 int main() {
-    double Attack_angle=Input_angle();
-    double Slip_angle=Input_angle();
+    double Attack_angle=Input_angle()*Pi/180;
+    double Slip_angle=Input_angle()*Pi/180;
+    double trans_matr[SIZE][SIZE];
+    memset(trans_matr,0,sizeof(trans_matr));
     vector velo_vec = Input_vector();
-    vector bound_vec =Velocity_to_bound(velo_vec, Attack_angle, Slip_angle);
+    Input_matrix (trans_matr,Attack_angle,Slip_angle);
+    vector bound_vec = Velocity_to_bound(velo_vec, trans_matr ,Attack_angle, Slip_angle);
     Show_vec(bound_vec);
 }
+
